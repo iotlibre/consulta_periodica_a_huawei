10aac2bf5a3686d6dad92b331427c5e0aceca9e5
onsar <oscar.puyal@gmail.com>
2023-08-31 15:35:37 +0200

    incluir logs

--- getStationRealKpi_v2.py
+++ getStationRealKpi_v2.py
@@ -19,11 +19,22 @@ import json;
 import configparser
 import paho.mqtt.publish as publish
 import sched, threading
+import logging
+from logging.handlers import RotatingFileHandler
 
+''' Niveles de logging
+Para obtener _TODO_ el detalle: level=logging.INFO
+Para comprobar los posibles problemas level=logging.WARNINg
+Para comprobar el funcionamiento: level=logging.DEBUG
+'''
+logging.basicConfig(
+        level=logging.DEBUG,
+        handlers=[RotatingFileHandler('./logs/log_datadis.log', maxBytes=10000000, backupCount=4)],
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p')
 
 def mqtt_tx(client,s_value):
     # logging.debug(client + "  " + s_register + "  " + s_value)
-    # Parseo de las variables
     mqtt_topic_prefix = parser.get('mqtt_broker','mqtt_topic_prefix')
     mqtt_ip = parser.get('mqtt_broker','mqtt_ip')
     mqtt_login = parser.get('mqtt_broker','mqtt_login')
@@ -31,7 +42,8 @@ def mqtt_tx(client,s_value):
 
     mqtt_auth = { 'username': mqtt_login, 'password': mqtt_password }
     response = publish.single(mqtt_topic_prefix + "/" + client, s_value, hostname=mqtt_ip, auth=mqtt_auth)
-
+    # logging.info( mqtt_ip + " -> " + mqtt_topic_prefix + "/" + client + "  " + s_value)
+    logging.info( mqtt_ip + " -> " + mqtt_topic_prefix + "/"  + client + "  " + str(s_value))
 
 def pedir_nuevo_key():
 
@@ -44,30 +56,30 @@ def pedir_nuevo_key():
     json_req_body = '{"userName":"'+huawei_login+'", "systemCode":"'+huawei_password+'" }';
 
     
-    print("headers:");
-    print(headers);
-    print("json_req_body:");
-    print(json_req_body);
+    logging.debug("headers:");
+    logging.debug(headers);
+    logging.debug("json_req_body:");
+    logging.debug(json_req_body);
     
     ''' Componemos y ejecutamos la peticion '''
     requestResponse = requests.post(urlOpenApiHuawei_login, data = json_req_body, headers=headers);
 
     # Obtenemos el codigo del estado devuelto por la peticion
     requestResponseStatus = requestResponse.status_code
-    print("Codigo de la respuesta obtenido:")
-    print(requestResponseStatus)
+    logging.debug("Codigo de la respuesta obtenido:")
+    logging.debug(requestResponseStatus)
     global HToken
     HToken = requestResponse.cookies.get("XSRF-TOKEN")
-    print("XSRF-TOKEN:")
-    print(type(HToken))
-    print(HToken)
+    logging.debug("XSRF-TOKEN:")
+    logging.debug(type(HToken))
+    logging.debug(HToken)
 
 
 # 
 parser = configparser.ConfigParser()
 parser.read('config_huawei_server.ini')
 pedir_nuevo_key()
-print(HToken)
+logging.debug(HToken)
 
 param_stationCodes = parser.get('huawei_inversor','stationCodes')
 inversorName = parser.get('huawei_inversor','name')
@@ -81,56 +93,54 @@ json_req_body = "{\"stationCodes\" : \""
 json_req_body += param_stationCodes
 json_req_body +="\"}"
 
-# print("headers:");
-# print(headers);
-print("json_req_body:");
-print(json_req_body);
+# logging.debug("headers:");
+# logging.debug(headers);
+logging.debug("json_req_body:");
+logging.debug(json_req_body);
 
+'''
 try:
 
-    ''' Componemos y ejecutamos la peticion '''
     requestResponse = requests.post(urlOpenApiHuawei_getDevRealKpi, data = json_req_body, headers=headers);
 
     # Obtenemos el codigo del estado devuelto por la peticion
     requestResponseStatus = requestResponse.status_code
-    #print("Codigo de la respuesta obtenido:");
-    #print(requestResponseStatus);
+    #logging.debug("Codigo de la respuesta obtenido:");
+    #logging.debug(requestResponseStatus);
 
     if requestResponseStatus==200:
         responseJson = json.loads(requestResponse.text)
-        print(responseJson['data'][0]["dataItemMap"])
+        # logging.debug(responseJson['data'][0]["dataItemMap"])
         r_value = responseJson['data'][0]["dataItemMap"]['total_power'] # received value
-        print('totalEnergy: ', r_value)
+        logging.debug("totalEnergy: " + str(r_value))
         mqtt_tx(inversorName,r_value)
 
 
 
 except Exception as ex:
-
-    print ("-----------");
-    print ("ERROR: LA EJECUCION NO HA TERMINADO CORRECTAMENTE DEBIDO A UN ERROR");
-    print (ex);
-    print ("-----------");
-
+    logging.debug ("-----------");
+    logging.debug ("ERROR: LA EJECUCION NO HA TERMINADO CORRECTAMENTE DEBIDO A UN ERROR");
+    logging.debug (ex);
+    logging.debug ("-----------");
+'''
 def serverReading(tm):
     threading.Timer(tm, serverReading,args=[tm]).start()
-    print("*" * 4 + ' serverReading')
+    logging.debug("*" * 4 + ' serverReading')
     try:
         ''' Componemos y ejecutamos la peticion '''
         requestResponse = requests.post(urlOpenApiHuawei_getDevRealKpi, data = json_req_body, headers=headers);
         requestResponseStatus = requestResponse.status_code
-        #print("Codigo respuesta: ",requestResponseStatus);
+        #logging.debug("Codigo respuesta: ",requestResponseStatus);
         if requestResponseStatus==200:
+            responseJson = json.loads(requestResponse.text)
             r_value = responseJson['data'][0]["dataItemMap"]['total_power'] # received value
-            print('totalEnergy: ', r_value)
+            logging.info('totalEnergy: ' + str(r_value))
             mqtt_tx(inversorName,r_value)
 
     except Exception as ex:
 
-        print ("-----------");
-        print ("ERROR: LA EJECUCION NO HA TERMINADO CORRECTAMENTE DEBIDO A UN ERROR");
-        print (ex);
-        print ("-----------");
+        logging.info ("ERROR: LA EJECUCION NO HA TERMINADO CORRECTAMENTE DEBIDO A UN ERROR");
+        logging.info (ex);
 
 serverReading(9.0)
 
